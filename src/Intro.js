516de6f3da82b4d0f04a1ec8d34a9b884921447b
leobr <leobrionesherrera@gmail.com>
2020-10-28 14:36:03 -0600

    Used @media on CSS to make site responsive

--- src/Intro.js
+++ src/Intro.js
@@ -4,33 +4,12 @@ import { Button } from 'react-bootstrap';
 import Fade from 'react-reveal/Fade';
 
 const Intro  = () => {
-    const [padding, setPadding] = useState(null);
-    const [fontSize, setFontSize] = useState(null);
-    const [textAlign, setTextAlign] = useState(null);
-
-
-    const styles = () => {
-            if (window.innerWidth > 769) {
-                setPadding("0rem 5.6rem")
-                setFontSize("3.5rem")
-                setTextAlign("left")
-            } else {
-                setPadding("0 1.6rem")
-                setFontSize("2.5rem")
-                setTextAlign("center")
-            }
-    }
-
-    useEffect(() => {
-        styles();
-        window.addEventListener('resize', styles)
-    }, []);
     
     return (
-        <section id="intro" style={{padding: padding}}>
+        <section id="intro" >
             <Container >
                 <Fade duration={1000} delay={500} distance="30px" left>
-                    <h1 className="intro-title" style={{fontSize: fontSize, textAlign: textAlign}}>
+                    <h1 className="intro-title" >
                         Hello! I'm Leonardo <br/>
                         I am a Full Stack Web Developer
                     </h1>
